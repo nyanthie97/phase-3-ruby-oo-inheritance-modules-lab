19c18846a37f516b9d1b370b98d9a371c25e6ba2
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-04 01:18:13 +0300

    commit tree 6472320f8a79b988522c828ea09c19c4a126fec8

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
   def initialize
     @@artists << self
     @songs = []
@@ -14,6 +16,9 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
@@ -31,7 +36,6 @@ class Artist
     @songs << song
     song.artist = self
   end
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
@@ -39,4 +43,9 @@ class Artist
   def to_param
     name.downcase.gsub(' ', '-')
   end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Paramable
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
   def initialize
     @@songs << self
   end
@@ -13,6 +15,9 @@ class Song
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -33,4 +38,8 @@ class Song
   def to_param
     name.downcase.gsub(' ', '-')
   end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
 end
